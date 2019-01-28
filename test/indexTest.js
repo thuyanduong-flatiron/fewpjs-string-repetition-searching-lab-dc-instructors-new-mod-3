df2459bed404cabd0717de3a61640da70c49a876
Kellye Greene <kellyecodes@gmail.com>
2019-01-28 15:15:42 -0500

    Adds tests

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,14 @@
-
-
-describe("index.html", () => {
-  it("includes 'hello'", () => {
-    expect(document.querySelector('body').innerHTML).to.include('hello')
+describe("heyJude", () => {
+  it("function heyJude repeats verse 16x", () => {
+    let verse = "Na na na na na na na, na na na na, hey Jude."
+    expect(heyJude(verse)).to.match(/Na na na na na na na, na na na na, hey Jude./)
   })
-
-  it("includes div", () => {
-    expect(document.querySelector('body').innerHTML, "no empty div element found within body").to.include('<div></div>')
-  })
-
-
-})
-
-
-describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-    expect(testFunc).to.exist
-    expect(testFunc()).to.equal('hi')
+  it("function 'prohibitedLanguage' bans 'candycorn' and 'brusselssprouts'", () => {
+    let safeName = "playernumberone"
+    let badName1 = "ilovecandycorn"
+    let badName2 = "brusselssproutsarenumberone"
+    expect(prohibitedLanguage(safeName)).to.eql(-1)
+    expect(prohibitedLanguage(badName1)).to.eql(5)
+    expect(prohibitedLanguage(badName2)).to.eql(0)
   })
 })
