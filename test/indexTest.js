b6a52854c7f53a014ea15b2137bee615c6bbb7b5
Kellye Greene <kellyecodes@gmail.com>
2019-01-28 15:13:52 -0500

    Finishes solution

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,9 +1,15 @@
 describe("heyJude", () => {
   it("function heyJude repeats verse 16x", () => {
-    expect(heyJude()).to.match(/Na na na na na na na, na na na na, hey Jude./)
+    let verse = "Na na na na na na na, na na na na, hey Jude."
+    expect(heyJude(verse)).to.match(/Na na na na na na na, na na na na, hey Jude./)
   })
-  it("has function 'prohibitedLanguage'", () => {
-    expect(prohibitedLanguage()).to.exist;
+  it("function 'prohibitedLanguage' bans 'candycorn' and 'brusselssprouts'", () => {
+    let safeName = "playernumberone"
+    let badName1 = "ilovecandycorn"
+    let badName2 = "brusselssproutsarenumberone"
+    expect(prohibitedLanguage(safeName)).to.eql(-1)
+    expect(prohibitedLanguage(badName1)).to.eql(5)
+    expect(prohibitedLanguage(badName2)).to.eql(0)
   })
 })
 
