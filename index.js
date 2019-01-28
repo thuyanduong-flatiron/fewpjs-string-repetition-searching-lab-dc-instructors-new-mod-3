f7aa48a5d18d6fb47b7c10a75e7c923d1a2f4807
Kellye Greene <kellyecodes@gmail.com>
2019-01-28 14:29:58 -0500

    WIP solution

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+heyJude = () => {
+  let verse = "Na na na na na na na, na na na na, hey Jude."
+  return verse.repeat(16)
+}
+
+prohibitedLanguage = () => {
+  var bannedWords = /candycorn|brusselssprouts/;
+  return string.search(bannedWords);  
+}
