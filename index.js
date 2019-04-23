cbffb1369f0d9bb46606062c0a7811f389190f0b
thuyanduong-flatiron <thuyanannduong@gmail.com>
2019-04-23 19:16:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function heyJude(){
+  return "Na na na na na na na, na na na na, hey Jude.".repeat(16)
+}
+
+function prohibitedLanguage(str){
+  let myRegExp = /candycorn|brusselssprouts/;
+  return str.search(myRegExp)
+}
